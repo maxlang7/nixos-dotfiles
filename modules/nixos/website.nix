f0bf57d04a415f18715de164f4b03b095bc1f6d5
Max <max@langhorst.com>
2025-02-25 10:37:00 -0500

    website stuff

--- modules/nixos/website.nix
+++ modules/nixos/website.nix
@@ -1,17 +1,35 @@
 {config, pkgs, ...}:
 {
-  imports =
-    [
-      ./desktop_environment_apps.nix
-      ./website.nix
-    ];
-  users.users.maxlang = {
-    isNormalUser = true;
-    description = "Max Langhorst";
-    extraGroups = [ "networkmanager" "wheel" "video" ];
-    packages = with pkgs; [
-      php
-      mariadb
-    ]   
+  services.nginx = {
+    enable = true;
+    virtualHosts."localhost" = {
+      root = "/home/maxlang/fish";
+      locations."~ \\.php$".extraConfig = '' 
+        fastcgi_pass 127.0.0.1:9000
+        fastfgi_index index.php;
+        include ${pkgs.nginx}/conf/fastcgi_params;
+      '';
+    };
   };
+  
+  virtualisation = {
+    podman = {
+      enable = true;
+      # Enable Docker compatibility
+      dockerCompat = true;
+    };
+    oci-containers.containers = {
+      fish = {
+        image = "php:8.2-fpm";
+        autoStart = true;
+        ports = [ "127.0.0.1:9000:9000" ];
+        volumes = [ "/home/maxlang/fish:/var/www/html" ];
+        extraOptions = [
+          "--network=host"
+        ];
+      };
+    };
+
+  };
+
 }
