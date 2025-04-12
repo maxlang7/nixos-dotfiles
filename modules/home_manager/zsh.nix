f9c4ca265056d7285c64757d05200481d58e7621
Max <max@langhorst.com>
2025-04-11 22:40:05 -0400

    little config changes (bat cat, zsh, hyprlock, waybar, hyprland)

--- modules/home_manager/zsh.nix
+++ modules/home_manager/zsh.nix
@@ -1,15 +1,19 @@
 {pkgs, ...}: 
 {
-  #environment.pathsToLink = [ "/share/zsh" ];
-  
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
+    # oh-my-zsh = {
+    #   enable = true;
+    #   theme = 
+    # };
     history = {
       append = true;
       extended = true;
       expireDuplicatesFirst = true;
+      ignoreDups = true;
+      ignoreSpace = true;
       save = 1000000;
       share = true;
       size = 1000000;
