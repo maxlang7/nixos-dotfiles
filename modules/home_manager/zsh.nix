1e075d73ef8a5752b71a3ab782fe12e896f511e8
Max <max@langhorst.com>
2025-05-07 11:32:29 -0400

    some zsh changes

--- modules/home_manager/zsh.nix
+++ modules/home_manager/zsh.nix
@@ -1,12 +1,12 @@
-{pkgs, ...}: 
+{pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     oh-my-zsh = {
-      enable = true;
-      theme = "";
+      enable = true; # Disable oh-my-zsh to avoid conflicts
+      #theme = "gruvbox"; # Commented out as oh-my-zsh is disabled
     };
     history = {
       append = true;
--- modules/nixos/terminal_utils.nix
+++ modules/nixos/terminal_utils.nix
@@ -12,14 +12,16 @@
     fzf
     fd
     ripgrep
-    ];
-  environment.pathsToLink = [ "/share/zsh" ];
+  ];
+  
   environment.shellAliases = {
     nrs = "sudo nixos-rebuild switch --flake /etc/nixos#Aragorn";
     td = "todoist-electron --enable-features=UseOzonePlatform --ozone-platform=wayland";
     tree = "cbonsai -l -m '\"I am at home among the trees\" - J.R.R Tolkien'";
     cat = "bat";
   };
+  
   programs.zsh.enable = true;
+  environment.pathsToLink = [ "/share/zsh" ];
   users.users.maxlang.shell = pkgs.zsh;
 }
