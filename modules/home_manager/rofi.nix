a053a67fc12dc112eba7bd1896f0a40b34de5fea
Max <max@langhorst.com>
2025-05-07 11:31:21 -0400

    Better rofi theme

--- modules/home_manager/rofi.nix
+++ modules/home_manager/rofi.nix
@@ -1,4 +1,5 @@
 {config, pkgs, ...}: 
 {
 	xdg.configFile."rofi/config.rasi".source = ../../artifacts/rofi/config.rasi;
+	xdg.configFile."rofi/gruvbox.rasi".source = ../../artifacts/rofi/gruvbox.rasi;
 }
