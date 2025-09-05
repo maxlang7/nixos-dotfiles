947e8806d4206d37ea48c0b3a3e197c597158b6c
Max <max@langhorst.com>
2025-09-05 11:50:04 -0400

    Since RTC battery is dead time will reset to 1980 on restart, breaking wifi validation, so I run a script on boot that sets the time at least somewhere in the ballpark of right so that my wifi will connect and then the time will become actually accurate. Bit janky but it is what it is without hardware.

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,5 +1,19 @@
 {pkgs,... }:
 {
   networking.hostName = "Aragorn";
-    networking.networkmanager.enable = true;
+  networking.networkmanager.enable = true;
+  systemd.services.wifi-time-fix = {
+    description = "Temporarily sets time to fix Wi-Fi certificate validation";
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.coreutils ];
+  
+    serviceConfig = {
+      User = "root";
+      Type = "oneshot"; 
+      ExecStart = "${pkgs.bash}/bin/bash /etc/nixos/artifacts/scripts/timeset.sh";
+      #FailureAction = "continue";
+      # Also, consider a timeout to prevent an infinite stall
+      TimeoutSec = "60s"; 
+    };
+  };
 }
