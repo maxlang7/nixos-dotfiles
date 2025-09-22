22f4cd6712e3923084182e234092108f65d0afa7
Max <max@langhorst.com>
2025-09-22 11:44:53 -0400

    Laundry timer

--- modules/home_manager/yazi.nix
+++ modules/home_manager/yazi.nix
@@ -1,7 +1,8 @@
-{...}: 
+{pkgs, pkgs-unstable, ...}: 
 {
 	programs.yazi = {
 		enable = true;
+		package = pkgs-unstable.yazi;
 		enableZshIntegration = true;
 	};
 	xdg.configFile."yazi/yazi.toml".source = ../../artifacts/yazi/yazi.toml;
