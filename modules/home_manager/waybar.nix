03f72c6bb76134348c414c5e59560172fd3ce9ef
Max <max@langhorst.com>
2025-09-22 12:33:17 -0400

    Laundry timer attempt 2

--- modules/home_manager/waybar.nix
+++ modules/home_manager/waybar.nix
@@ -2,7 +2,5 @@
 {
 	xdg.configFile."waybar/config.jsonc".source = ../../artifacts/waybar/config.jsonc;
 	xdg.configFile."waybar/style.css".source = ../../artifacts/waybar/style.css;
-  xdg.configFile."uair/uair_laundry.toml".source = ../../artifacts/uair/uair_laundry.toml;
-  xdg.configFile."uair/uair_pomodoro.toml".source = ../../artifacts/uair/uair_pomodoro.toml;
-
+  xdg.configFile."uair/uair.toml".source = ../../artifacts/uair/uair.toml;
 }
