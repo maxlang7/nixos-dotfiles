5b797f46dcd7c1665ef504a41819f5ffa5c3ff64
Max <max@langhorst.com>
2025-09-22 22:16:23 -0400

    more cleanup

--- modules/home_manager/rofi.nix
+++ modules/home_manager/rofi.nix
@@ -1,4 +1,4 @@
-{config, pkgs, ...}: 
+{...}: 
 {
 	xdg.configFile."rofi/config.rasi".source = ../../artifacts/rofi/config.rasi;
 	xdg.configFile."rofi/gruvbox.rasi".source = ../../artifacts/rofi/gruvbox.rasi;
