bde2a6c279d94f3dfac7561fd97d43ff275b0ad3
Max <max@langhorst.com>
2025-09-30 23:04:48 -0400

    Finalizing music setup

--- modules/nixos/navidrome.nix
+++ modules/nixos/navidrome.nix
@@ -6,7 +6,7 @@
     settings = {
       MusicFolder = "/home/maxlang/Music/navidrome";
       EnableSharing = true;
-      UIWelcomeMessage = "Testing";
+      UIWelcomeMessage = "Musik";
       LastFM.ApiKey = "194e34c66e3815c10e97baca8f6f5ac6";
       LastFM.Secret = "36e02bd964081a5937875b82d6ca9c26";
     };
