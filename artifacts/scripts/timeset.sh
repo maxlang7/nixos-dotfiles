20a6396cf8653dc60c1a22ea53e67f3cba053988
Max <max@langhorst.com>
2026-02-08 15:44:00 -0500

    Update timeset. Should really be automated cause now everyone few months my time gets out of sync if I shutoff my computer lol. Timezones also need work

--- artifacts/scripts/timeset.sh
+++ artifacts/scripts/timeset.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Define the target date and time
-TARGET_DATE="2025-09-23 09:00:00"
+TARGET_DATE="2026-01-15 09:00:00"
 
 # Convert the target date to a Unix timestamp
 TARGET_TIMESTAMP=$(date -d "$TARGET_DATE" +%s)
@@ -12,11 +12,11 @@ CURRENT_TIMESTAMP=$(date +%s)
 # Check if the current date is before the target date
 if (( CURRENT_TIMESTAMP < TARGET_TIMESTAMP )); then
     echo "Current date is before August 1, 2025. Setting system time..."
-    
+
     # Set the system date and time
     # This command requires superuser privileges
     date -s "$TARGET_DATE"
-    
+
     echo "Date and time have been set to 08/01/2025 09:00:00."
     echo "Restaring network manager"
     systemctl restart NetworkManager
