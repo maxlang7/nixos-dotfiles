7a75aaadd64da6fd790c33e91442e7587b399d77
Max <max@langhorst.com>
2026-03-25 15:32:11 -0400

    minimal yazi keybind (only overriding default for one thing)

--- modules/home_manager/yazi.nix
+++ modules/home_manager/yazi.nix
@@ -1,4 +1,4 @@
-{pkgs, pkgs-unstable, ...}: 
+{pkgs-unstable, ...}: 
 {
 	programs.yazi = {
 		enable = true;
@@ -6,6 +6,6 @@
 		enableZshIntegration = true;
 	};
 	xdg.configFile."yazi/yazi.toml".source = ../../artifacts/yazi/yazi.toml;
-	xdg.configFile."yazi/keymap.toml".source = ../../artifacts/yazi/keyamp.toml;
+	xdg.configFile."yazi/keymap.toml".source = ../../artifacts/yazi/keymap.toml;
 
 }
