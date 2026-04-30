55eacc7eb1c2a81d1b633a7c643c23b023d877e6
Max <max@langhorst.com>
2026-04-30 12:12:20 -0400

    fixing zed config in nix

--- modules/home_manager/zed.nix
+++ modules/home_manager/zed.nix
@@ -1,73 +1,62 @@
-{pkgs-unstable, ...}: 
+{pkgs-unstable, ...}:
 {
   programs.zed-editor = {
     enable = true;
-  
+    package = pkgs-unstable.zed-editor-fhs;
     userSettings = {
-      context_servers = {
-        "fixed-claude" = {
-          enabled = true;
-          remote = false;
-          command = "claude-agent-acp";
-          args = [];
-          env = {};
-        };
-      };
-  
       autosave = {
         after_delay = {
           milliseconds = 1000;
         };
       };
-  
-      agent_servers = {};
-  
+
+      agent_servers = {
+        claude-acp = {
+          type = "registry";
+        };
+      };
+
       icon_theme = {
         mode = "dark";
         light = "Zed (Default)";
-        dark = "Zed (Default)";
+        dark = "Material Icon Theme";
       };
-  
+
       session = {
         trust_all_worktrees = true;
       };
-  
+
       ui_font_size = 16;
       buffer_font_size = 15;
-  
+
       theme = {
         mode = "dark";
         light = "One Light";
         dark = "Gruvbox Dark";
       };
-  
+
       project_panel = {
         dock = "left";
       };
-  
+
       outline_panel = {
         dock = "left";
       };
-  
+
       collaboration_panel = {
         dock = "right";
       };
-  
+
       agent = {
         dock = "right";
         favorite_models = [];
         model_parameters = [];
       };
-  
+
       git_panel = {
         dock = "left";
         sort_by_path = false;
       };
     };
-  
-    # Make sure claude-agent-acp is available
-    extraPackages = with pkgs-unstable; [
-      claude-agent-acp
-    ];
   };
 }
