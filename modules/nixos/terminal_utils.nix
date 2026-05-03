6beb663e90de464107e1d18cbb8ad2f4d0544019
Max <max@langhorst.com>
2026-05-03 16:13:03 -0400

    shell alias to download music

--- modules/nixos/terminal_utils.nix
+++ modules/nixos/terminal_utils.nix
@@ -20,6 +20,7 @@
     tree = "cbonsai -l -m '\"I am at home among the trees\" - J.R.R Tolkien'";
     cat = "bat";
     ff = "clear && fastfetch";
+    dm = "yt-dlp -x --audio-format m4a --download-archive \"/home/maxlang/Music/ytdl/archive.txt\" -o \"/home/maxlang/Music/ytdl/%(title)s.%(ext)s\" \"https://www.youtube.com/playlist?list=PLTIbe5JVteQdg_7yAHxcl-WcONflu1uEQ\"";
     nrsu = "sudo nix flake update --flake /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos#${hostName}";
   };
 
