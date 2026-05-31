2c975b7006d6eec5f84b025ea84e0c4d857ae0a8
Max <max@langhorst.com>
2026-05-31 01:41:33 -0400

    update, no more syncthing, no need for navidrome (setup on up now)

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -3,39 +3,32 @@
 
   services.syncthing = {
     enable = true;
-    user = "${user}"; # Replace with your actual NixOS username
-    dataDir = "/home/${user}";
-    openDefaultPorts = true;
+    user = "${user}";
+    dataDir = "/home/${user}/Music/navidrome";
+    configDir = "/home/${user}/.config/syncthing";
+    openDefaultPorts = true; # Opens firewall ports 22000/TCP/UDP and 21027/UDP
+
+    overrideDevices = true;     # Overrides any devices added manually via WebUI
+    overrideFolders = true;     # Overrides any folders added manually via WebUI
+
+    settings = {
+      devices = {
+        "Up" = { id = "5UMGQQ6-3N3DXSU-F4PI5Z6-ON6SHAP-ACDIVYH-FURDVR4-X5V7ED6-TOUQJAS"; };
+      };
+      folders = {
+        "navidrome" = {
+          path = "/home/${user}/Music/navidrome"; # Path to your laptop music folder
+          devices = [ "Up" ];         # Share this folder with the server
+          versioning = {
+            type = "simple";
+            params = { keep = "5"; };        # Keeps 5 old versions of modified/deleted tracks
+          };
+        };
+      };
+    };
   };
 
-  # services.syncthing = {
-  #   enable = true;
-  #   user = "${user}";
-  #   dataDir = "/home/${user}/Music/navidrome";
-  #   configDir = "/home/${user}/.config/syncthing";
-  #   openDefaultPorts = true; # Opens firewall ports 22000/TCP/UDP and 21027/UDP
-
-  #   overrideDevices = true;     # Overrides any devices added manually via WebUI
-  #   overrideFolders = true;     # Overrides any folders added manually via WebUI
-
-  #   settings = {
-  #     devices = {
-  #       "Up" = { id = "SERVER-DEVICE-ID-HERE..."; };
-  #     };
-  #     folders = {
-  #       "navidrome" = {
-  #         path = "/home/${user}/Music/navidrome"; # Path to your laptop music folder
-  #         devices = [ "Up" ];         # Share this folder with the server
-  #         versioning = {
-  #           type = "simple";
-  #           params = { keep = "5"; };        # Keeps 5 old versions of modified/deleted tracks
-  #         };
-  #       };
-  #     };
-  #   };
-  # };
-
-  # # Force Syncthing to skip creating its annoying default "~/Sync" folder
-  # systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
+  # Force Syncthing to skip creating its annoying default "~/Sync" folder
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
 
 }
