ead6135fe9e785660a5b280fce2a1b49a153ef7e
Max <max@langhorst.com>
2026-06-18 11:38:42 -0400

    update/add packages

--- modules/nixos/terminal_utils.nix
+++ modules/nixos/terminal_utils.nix
@@ -22,6 +22,7 @@
     ff = "clear && fastfetch";
     dm = "yt-dlp -x --audio-format m4a --download-archive \"/home/maxlang/Music/ytdl/archive.txt\" -o \"/home/maxlang/Music/ytdl/%(title)s.%(ext)s\" \"https://www.youtube.com/playlist?list=PLTIbe5JVteQdg_7yAHxcl-WcONflu1uEQ\"";
     nrsu = "sudo nix flake update --flake /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos#${hostName}";
+    ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
   };
 
   programs.zsh.enable = true;
