c1453c6c595f71770271a51deb35cb4d87669a45
Max <max@langhorst.com>
2026-07-16 14:35:29 -0400

    feat(syncthing): sync ~/Documents/books

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -1,34 +1,36 @@
 {user, ...}:
 {
-
   services.syncthing = {
     enable = true;
     user = "${user}";
-    dataDir = "/home/${user}/Music/navidrome";
+    dataDir = "/home/${user}/";
     configDir = "/home/${user}/.config/syncthing";
-    openDefaultPorts = true; # Opens firewall ports 22000/TCP/UDP and 21027/UDP
-
-    overrideDevices = true;     # Overrides any devices added manually via WebUI
-    overrideFolders = true;     # Overrides any folders added manually via WebUI
-
+    openDefaultPorts = true;
+    overrideDevices = true;
+    overrideFolders = true;
     settings = {
       devices = {
         "Up" = { id = "5UMGQQ6-3N3DXSU-F4PI5Z6-ON6SHAP-ACDIVYH-FURDVR4-X5V7ED6-TOUQJAS"; };
       };
       folders = {
         "navidrome" = {
-          path = "/home/${user}/Music/navidrome"; # Path to your laptop music folder
-          devices = [ "Up" ];         # Share this folder with the server
+          path = "/home/${user}/Music/navidrome";
+          devices = [ "Up" ];
           versioning = {
             type = "simple";
-            params = { keep = "5"; };        # Keeps 5 old versions of modified/deleted tracks
+            params = { keep = "5"; };
+          };
+        };
+        "books" = {
+          path = "/home/${user}/Documents/books";
+          devices = [ "Up" ];
+          versioning = {
+            type = "simple";
+            params = { keep = "5"; };
           };
         };
       };
     };
   };
-
-  # Force Syncthing to skip creating its annoying default "~/Sync" folder
   systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
-
 }
